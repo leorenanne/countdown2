1fa6bf0e0760a4e0d8d72ef4790ca50625398b28
Leoren Tanyag <leoren.tanyag@rea-group.com>
2017-07-06 16:45:59 +1000

    Move main event snippets from App to MainEvent component

--- src/App.js
+++ src/App.js
@@ -5,28 +5,10 @@ import MainEvent from './components/MainEvent'
 import Events from './components/Events'
 
 class App extends Component {
-  constructor(){
-    super();
-
-    this.state = {
-      name: undefined,
-      date: undefined,
-    }
-  }
-
-  componentDidMount() {
-    var mainEvent = localStorage.getItem("mainEvent") ? 
-                    JSON.parse(localStorage.getItem("mainEvent")) :
-                    {name: "Today", date: (new Date()).toJSON() };
-
-    this.setState({date: mainEvent.date });
-    this.setState({name: mainEvent.name });
-  }
-
   render() {
     return (
       <div>
-        <MainEvent mainEvent={this.state.name} date={this.state.date}/>
+        <MainEvent />
         <Events />
       </div>
     );
--- src/components/MainEvent.js
+++ src/components/MainEvent.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import DaysLeft from './DaysLeft'
 import '../../public/stylesheets/components/MainEvent.css';
 
 class MainEvent extends React.Component {
+  constructor(){
+    super();
 
-  static propTypes = {
-    mainEvent: PropTypes.string,
-    date: PropTypes.string
-  };
-  static defaultProps = {
-    mainEvent: 'Today',
-    date: (new Date()).toJSON()
+    this.state = {
+      name: undefined,
+      date: undefined,
+    }
+  }
+
+  componentDidMount() {
+    var mainEvent = localStorage.getItem("mainEvent") ? 
+                    JSON.parse(localStorage.getItem("mainEvent")) :
+                    {name: "Today", date: (new Date()).toJSON() };
+                    
+    this.setState({date: mainEvent.date });
+    this.setState({name: mainEvent.name });
   }
 
   render() {
     return (
       <div>
         <span className='main-event'>
-          <span className="main-event-name">{this.props.mainEvent}</span>
-          <span className='main-event-days'><DaysLeft dateString={this.props.date}/></span>
+          <span className="main-event-name">{this.state.name}</span>
+          <span className='main-event-days'><DaysLeft dateString={this.state.date}/></span>
           <span>DAYS</span>
         </span>
       </div>
